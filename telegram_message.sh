5eeb9cddefc72b92bb7beb61ed4988781293c2a6
Andy Partington <andy.partington@gmail.com>
2015-04-16 13:15:32 +0100

    Added explanations
    Added explanations on usage of different areas

--- telegram_message.sh
+++ telegram_message.sh
@@ -3,4 +3,5 @@ NAGMSG=$1
 #Path to Telegram CLI
 cd /home/nagios/telegram
 #Need to do it this way to give the CLI to respond, the -W activates the session, could do echo 'contact_list' or something instead, this maybe improved upon 
+# Used Safe_quit as that is what the developer recommends, you can drop the -W if you 'msg user#userID message' as it doesn't have to look up the user.
 (sleep 5; echo "msg <user to message> $NAGMSG"; echo 'safe_quit';) | bin/telegram-cli -W
